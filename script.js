c0966e98c7f57839d8120b4cf78fd0152d42d190
earnwithgudiya <gudiya1832005@gmail.com>
2026-07-06 11:49:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll("a").forEach(link=>{
+link.addEventListener("click",function(e){
+const id=this.getAttribute("href");
+if(id.startsWith("#")){
+e.preventDefault();
+document.querySelector(id).scrollIntoView({
+behavior:"smooth"
+});
+}
+});
+});
+
+window.addEventListener("load",()=>{
+document.body.style.opacity="1";
+});
